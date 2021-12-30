adb2734b7baa4bad4d050c4b969f3fcc6b392cd5
mayke.furtado <mayke.furtado@arcom.com.br>
2021-12-30 08:15:14 -0300

    feat: init

---
+++ src/main/java/br/com/maykealisson/ceplu/CepluApplication.java
@@ -0,0 +1,13 @@
+package br.com.maykealisson.ceplu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/maykealisson/ceplu/CepluApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maykealisson.ceplu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
